1c82a91ccd42ed9c11fa884e6ff0a2fd0b9d6f4a
dstaraster1969 <71042914+dstaraster1969@users.noreply.github.com>
2023-07-25 07:27:37 -0700

    working

--- filter.py
+++ filter.py
@@ -1,22 +1,34 @@
 import json
 
 
-def filter_data(j, f):
+def filter_all(json_objs, filters):
+    # We want to run the first filter on all the json objects
+    results = apply_filter(json_objs, filters[0])
+
+    # For the rest of the filters, we want to feed it just the json objects that were just returned
+    for f in filters:
+        results = apply_filter(results, f)
+    return results
+
+
+def apply_filter(json_objs, f):
+    results = []
+    for j in json_objs:
+        if check_for_match(j, f):
+            results.append(j)
+
+    return results
+
+
+def check_for_match(j, f):
     if f == {}:
         return True
     try:
         key = list(f.keys())[0]
         if not type(f[key]) == dict:
-            return j[key] == f[key]
-        return filter_data(j[key], f[key])
-    except:
+            if j[key] == f[key]:
+                return True
+        else:
+            check_for_match(j[key], f[key])
+    except KeyError:
         return False
-
-
-def filter_all(json_objs, filters):
-    results = []
-    for j in json_objs:
-        x = filter_data(j, json.loads(filters))
-        if x:
-            results.append(j)
-    return results
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import asyncio
 import aiohttp
 import sys
+import json
 
 from helpers import parse_args, get_data_from_api
-from filter import filter_all, filter_data
+from filter import filter_all
 
 
 def main():
     data = get_data_from_api(num_pages)
-    filtered_data = filter_all(data, json_filter)
+    filtered_data = filter_all(data, json.loads(json_filter))
+    formatted = json.dumps(filtered_data, indent=2)
     print(filtered_data)
 
 
